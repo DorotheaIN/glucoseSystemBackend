0373fee3798ead5d8bd6747a52aa3bf8f4c80c0b
i527826 <dorothea.zhao@sap.com>
2023-04-27 14:53:19 +0800

    init

---
+++ src/test/java/com/example/glucosesystembackend/GlucoseSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glucosesystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlucoseSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
